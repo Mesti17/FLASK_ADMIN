7577676e292104a4e8f76ee8d0a6da7e2d51e4f4
Mesti17 <mesty017@gmail.com>
2023-06-24 14:03:59 +0700

    Update code for getting scraping data for each candidate

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from scraping_data import scraping_tweet
+# from scraping_data import scraping_tweet
+from scraping_data_new import search_tweets_ganjar, search_tweets_anies, search_tweets_prabowo
 from gabung_data import gabungdata
 
 app = Flask(__name__)
@@ -19,9 +20,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/scraping", methods=['GET', "POST"])
-def scrapingTweet():
-    scraping_tweet()
+@app.route("/scraping_prabowo", methods=['GET', "POST"])
+def scrapingTweetPrabowo():
+    search_tweets_prabowo()
+    return redirect(url_for("index"))
+
+
+@app.route("/scraping_ganjar", methods=['GET', "POST"])
+def scrapingTweetGanjar():
+    search_tweets_ganjar()
+    return redirect(url_for("index"))
+
+
+@app.route("/scraping_anies", methods=['GET', "POST"])
+def scrapingTweetAnies():
+    search_tweets_anies()
     return redirect(url_for("index"))
 
 
---
+++ scraping_data_new.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import snscrape.modules.twitter as sntwitter
+import tweepy
+import pandas as pd
+import re
+import os
+
+path = "data_scraping"
+
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+
+def search_tweets_prabowo():
+    data_prabowo = []
+
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper("Prabowo Subianto").get_items()):
+        if i > 100:
+            break
+        data_prabowo.append([tweet.user.username, tweet.content])
+
+        # db.commit()
+
+    df = pd.DataFrame(data_prabowo, columns=['Username', "Tweet"])
+    df.to_csv(os.path.join(path, "scraping_prabowo.csv"),
+              index=False, encoding="utf-8")
+
+
+def search_tweets_ganjar():
+    data_ganjar = []
+
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper("Ganjar Pranowo").get_items()):
+        if i > 100:
+            break
+        data_ganjar.append([tweet.user.username, tweet.content])
+
+        # db.commit()
+
+    df = pd.DataFrame(data_ganjar, columns=['Username', "Tweet"])
+    df.to_csv(os.path.join(path, "scraping_ganjar.csv"),
+              index=False, encoding="utf-8")
+
+
+def search_tweets_anies():
+    data_anies = []
+
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper("Anies Baswedan").get_items()):
+        if i > 100:
+            break
+        data_anies.append([tweet.user.username, tweet.content])
+
+        # db.commit()
+
+    df = pd.DataFrame(data_anies, columns=['Username', "Tweet"])
+    df.to_csv(os.path.join(path, "scraping_anies.csv"),
+              index=False, encoding="utf-8")
